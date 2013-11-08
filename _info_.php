0984efc410aea24b6c0b3013806dd795b2cfbe3e
xtr4nge <xtr4nge@gmail.com>
2013-11-08 10:40:58 +0000

    Update _info_.php

--- _info_.php
+++ _info_.php
@@ -1,6 +1,6 @@
 <?
 $mod_name="dnsspoof";
-$mod_version="1.1";
+$mod_version="1.2";
 $mod_logs="/usr/share/FruityWifi/logs/dnsspoof.log"; 
 $mod_logs_history="/usr/share/FruityWifi/www/modules/dnsspoof/includes/logs/"; 
 ?>
