38efaccb28ffc04aac47a5ddfdcf898696920ba1
xtr4nge <xtr4nge@gmail.com>
2014-02-28 22:51:50 +0000

    version has been changed

--- _info_.php
+++ _info_.php
@@ -1,6 +1,6 @@
 <?
 $mod_name="dnsspoof";
-$mod_version="1.3";
+$mod_version="1.4";
 $mod_logs="/usr/share/FruityWifi/logs/dnsspoof.log"; 
 $mod_logs_history="/usr/share/FruityWifi/www/modules/dnsspoof/includes/logs/";
 $mod_panel="show";
