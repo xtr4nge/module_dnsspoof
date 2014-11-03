8cc42ed106cddb5d21bdc701485c5cfe52a15a59
xtr4nge <xtr4nge@gmail.com>
2014-11-03 14:29:54 -0300

    installation script has been updated

--- _info_.php
+++ _info_.php
@@ -1,6 +1,6 @@
 <?
 $mod_name="dnsspoof";
-$mod_version="1.5";
+$mod_version="1.6";
 $mod_logs="/usr/share/FruityWifi/logs/dnsspoof.log"; 
 $mod_logs_history="/usr/share/FruityWifi/www/modules/dnsspoof/includes/logs/";
 $mod_panel="show";
@@ -8,6 +8,7 @@ $mod_isup="ps auxww | grep dnsspoof | grep -v -e grep";
 $mod_alias="DNSspoof";
 # EXEC
 $bin_danger = "/usr/share/FruityWifi/bin/danger";
+$bin_sudo = "/usr/bin/sudo";
 $bin_dnsspoof = "/usr/sbin/dnsspoof";
 $bin_killall = "/usr/bin/killall";
 $bin_echo = "/bin/echo";
